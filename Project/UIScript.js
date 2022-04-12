31a3707414ee7e8a5bd5f1da0f91616cfb5a017f
Anirudh Sharma <mcxiv14@gmail.com>
2022-04-12 19:18:03 +0530

    update jskey completed

--- Project/UIScript.js
+++ Project/UIScript.js
@@ -1,92 +1,50 @@
 // noinspection JSJQueryEfficiency
 
-const fr = new FileReader();
-fr.onload = function () {
-    document.getElementById('output')
-        .textContent = fr.result;
-}
-fr.readAsText('');
-
 if ($(window).width() > 800) {
 
+    const rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "https://raw.githubusercontent.com/Minecraftian14/KonnexxionsHomeworks/project/Project/UICustomScript.jskey", false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status === 0) {
+                let allText = rawFile.responseText.split("\n");
 
-    $(".div_band_a").click(function func() {
-        $(this).css("width", "91%");
-        $(".div_content_place").css("opacity", "0");
-        $(".div_band_a").css("opacity", "1");
-        $(".div_band_c").css("width", "91%");
-        $(".div_band_b").css("width", "91%");
-        $(".overlay_div_which_i_sincerely_hate").css("cursor", "pointer");
-        $(".overlay_div_which_i_sincerely_hate").css("display", "block");
-    });
+                for (let i = 0; i < allText.length; i++) {
+                    if (allText[i].endsWith(":")) {
 
-    $(".div_band_c").click(function () {
-        $(this).css("width", "91%");
-        $(".div_band_b").css("width", "3%");
-        $(".div_band_a").css("width", "3%");
-        $(".div_content_place").css("opacity", "0");
-        $(".div_band_c").css("opacity", "1");
-        $(".overlay_div_which_i_sincerely_hate").css("cursor", "pointer");
-        $(".overlay_div_which_i_sincerely_hate").css("display", "block");
-    });
+                        let array = allText[i].split(" > ");
+                        let name_ref = array[0]
+                        let func_ref = array[1]
 
-    $(".overlay_div_which_i_sincerely_hate").click(function () {
-        $(".div_content_place").css("width", "91%");
-        $(".div_band_c").css("width", "3%");
-        $(".div_content_place").css("opacity", "1");
-        $(".div_content_place").css("cursor", "");
-        $(".overlay_div_which_i_sincerely_hate").css("display", "none");
-    });
+                        let property_value_triplets = []
 
-    $(".div_band_b").click(function () {
-        $(this).css("width", "91%");
-        $(".div_content_place").css("opacity", "0");
-        $(".div_band_b").css("opacity", "1");
-        $(".div_band_a").css("width", "3%");
-        $(".div_band_c").css("width", "91%");
-        $(".overlay_div_which_i_sincerely_hate").css("cursor", "pointer");
-        $(".overlay_div_which_i_sincerely_hate").css("display", "block");
-    });
+                        while (allText[++i].trim() !== "") {
+                            array = allText[i].split(".")
+                            let name = array[0].trim()
+                            if (!name.startsWith("#"))
+                                name = '.' + name;
+                            let func = array[1]
+                            array = array[2].split("=")
+                            let property = array[0].trim()
+                            let value = array[1].trim()
 
-    $(".p_heading span,.horizontal_item p,.fab,.fas").hover(function () {
-        $(this).css("cursor", "pointer");
-    });
+                            property_value_triplets.push([name, property, value])
+                        }
 
-    $(".overlay_div_which_i_sincerely_hate").click(function () {
-        $(".div_content_place").css("width", "91%");
-        $(".div_band_c").css("width", "3%");
-        $(".div_content_place").css("opacity", "1");
-        $(".div_content_place").css("cursor", "");
-        $(".overlay_div_which_i_sincerely_hate").css("display", "none");
-    });
+                        function behaviour() {
+                            property_value_triplets.forEach(triplet => {
+                                if (triplet[0] === ".this") $(this).css(triplet[1], triplet[2]);
+                                else $(triplet[0]).css(triplet[1], triplet[2]);
+                            })
+                        }
 
-    $("#wavy_underlined_text,.horizontal_item p").click(function () {
-        $("#band_a").css("width", "91%");
-        $(".div_band_a").css("width", "85%");
-        $(".div_content_place").css("opacity", "0");
-        $(".div_band_a").css("opacity", "1");
-        $(".div_band_c").css("width", "85%");
-        $(".div_band_b").css("width", "85%");
-        $(".overlay_div_which_i_sincerely_hate").css("cursor", "pointer");
-        $(".overlay_div_which_i_sincerely_hate").css("display", "block");
-    });
+                        if (func_ref === "click:") $(name_ref).click(behaviour);
+                        else if (func_ref === "hover:") $(name_ref).hover(behaviour);
 
-    $(".overlay_div_which_i_sincerely_hate").click(function () {
-        $(".div_content_place").css("width", "91%");
-        $(".div_band_a").css("width", "3%");
-        $(".div_band_c").css("width", "3%");
-        $(".div_band_b").css("width", "3%");
-        $(".div_content_place").css("opacity", "1");
-        $(".div_content_place").css("cursor", "");
-        $(".overlay_div_which_i_sincerely_hate").css("display", "none");
-    });
-
-    $(".overlay_div_which_i_sincerely_hate").click(function () {
-        $(".div_content_place").css("width", "91%");
-        $(".div_band_b").css("width", "3%");
-        $(".div_content_place").css("opacity", "1");
-        $(".div_content_place").css("cursor", "");
-        $(".div_band_c").css("width", "3%");
-        $(".overlay_div_which_i_sincerely_hate").css("display", "none");
-    });
+                    }
+                }
+            }
+        }
+    }
+    rawFile.send(null);
 }
